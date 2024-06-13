1fb6dd91d9738930b9f2c6c013e1ee8d6d5a90de
kazurs <kwbrtpymt+git@gmail.com>
2024-06-13 23:15:25 +0900

    Initial implementation of callbacks

--- aisaac_raspi_relay/src/aisaac_raspi_relay.cpp
+++ aisaac_raspi_relay/src/aisaac_raspi_relay.cpp
@@ -3,13 +3,32 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "aisaac_msgs/msg/aisaac_strategy.hpp"
+#include "aisaac_msgs/msg/aisaac_referee.hpp"
+#include "aisaac_msgs/msg/aisaac_manual_control.hpp"
+using std::placeholders::_1;
 
 class AisaacRaspiRelay : public rclcpp::Node
 {
 public:
     AisaacRaspiRelay() : Node("aisaac_raspi_relay")
-    {}
+    {
+        strategy_ = this->create_subscription<aisaac_msgs::msg::AisaacStrategy>("strategy", 10, std::bind(&AisaacRaspiRelay::strategy_callback, this, _1));
+        referee_ = this->create_subscription<aisaac_msgs::msg::AisaacReferee>("referee", 10, std::bind(&AisaacRaspiRelay::referee_callback, this, _1));
+        manual_control_ = this->create_subscription<aisaac_msgs::msg::AisaacManualControl>("manual_control", 10, std::bind(&AisaacRaspiRelay::manual_control_callback, this, _1));
+    }
 private:
+    void strategy_callback(const aisaac_msgs::msg::AisaacStrategy::SharedPtr msg) const {
+        RCLCPP_INFO(this->get_logger(), "RX: '%d'", msg->robot_id);
+    }
+    void referee_callback(const aisaac_msgs::msg::AisaacReferee::SharedPtr msg) const {
+        RCLCPP_INFO(this->get_logger(), "RX: '%d'", msg->in_game);
+    }
+    void manual_control_callback(const aisaac_msgs::msg::AisaacManualControl::SharedPtr msg) const {
+        RCLCPP_INFO(this->get_logger(), "RX: '%d'", msg->robot_id);
+    }
+    rclcpp::Subscription<aisaac_msgs::msg::AisaacStrategy>::SharedPtr strategy_;
+    rclcpp::Subscription<aisaac_msgs::msg::AisaacReferee>::SharedPtr referee_;
+    rclcpp::Subscription<aisaac_msgs::msg::AisaacManualControl>::SharedPtr manual_control_;
 };
 
 int main(int argc, char *argv[])
